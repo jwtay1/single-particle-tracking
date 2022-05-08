25b3bb1c7176e568ca6195fbdb2525ef4d380360
Jian Tay <jian.tay@colorado.edu>
2022-05-08 11:05:05 -0600

    Added test code

--- simulation/runTest.m
+++ simulation/runTest.m
@@ -1,2 +1,43 @@
-randomwalk
-computeMSD
+clearvars
+clc
+
+simulateRandomWalk('test.xls')
+
+%%
+trackData = readDataFromXLS(...
+    'test.xls', ...
+    'sim data', 1);
+
+
+[T, SD] = calculateSDandLagTime(trackData);
+
+lagTimes = unique(T);
+
+%Plot histogram of 1, 2, 3, 4 deltaT
+for ii = 1:5
+
+    histogram(SD(T == lagTimes(ii)), 'binWidth', 0.0001)
+    hold on
+
+end
+
+hold off
+
+%%
+
+%Compute the MSD
+for ii = 1:numel(lagTimes)
+
+    MSD(ii) = mean(SD(T == lagTimes(ii)));
+
+end
+
+plot(lagTimes, MSD)
+
+%Fit the MSD to calculate the diffusion coefficient
+%MSD = 2nDt
+fitData = fit(lagTimes(1:4)', MSD(1:4)', 'poly1');
+
+D = fitData.p1, 
+
+
---
+++ simulation/simulateRandomWalk.m
@@ -0,0 +1,73 @@
+function simulateRandomWalk(outputFN, varargin)
+%SIMULATERANDOMWALK  Generate simulated random walk data
+%
+%  SIMULATERANDOMWALK(XLSFILE, PARAMS) will generate data of single
+%  particles undergoing a random walk to validate the MSD analysis code.
+%  The data will be saved as an XLS file with similar headers to mimic test
+%  data sets.
+%
+%  PARAMS is a struct that contains parameters of the simulation. The
+%  following are parameters (default values in parentheses):
+%      stepSize - Average step size in arb. units (2)
+%      numParticles - Number of tracks to simulate (50)
+%      timeStep - Time between frames (0.07)
+%      trackLength - Length of each track (30)
+
+ip = inputParser;
+addParameter(ip, 'stepSize', 2);
+addParameter(ip, 'numParticles', 50);
+addParameter(ip, 'timeStep', 0.07);
+addParameter(ip, 'trackLength', 30);
+parse(ip, varargin{:})
+
+%Columns needed: frame, timestamp, x(nm), y(nm), z(nm), x sigma [nm]	y sigma [nm]	z sigma [nm]	photons	bkg photons	track	track for matlab	track length
+
+particleData = cell(1, ip.Results.numParticles);
+
+for iParticle = 1:ip.Results.numParticles
+
+    %Generate a vector of random motion
+    vDirection = rand(ip.Results.trackLength - 1, 1) * 2 * pi;
+
+    %Generate step sizes using a normal distribution. Mean is the step
+    %size. Currently assuming std = 1.
+    dStep = randn(ip.Results.trackLength - 1, 1) + ip.Results.stepSize;
+
+    %Convert the above into Cartesian coordinates
+    dStepXY = [dStep .* cos(vDirection), dStep .* sin(vDirection)];
+
+    %Generate a random starting position
+    dStepXY = [rand(1), rand(1); dStepXY];
+
+    %Add the previous position to each to get the final vector of
+    %displacements
+    particlePos = cumsum(dStepXY, 1);
+
+    %Generate the columns as matrices
+    particleData{iParticle} = [(1:ip.Results.trackLength)', ...
+        (1:ip.Results.trackLength)' * ip.Results.timeStep, ...
+        particlePos(:, 1), particlePos(:, 2), zeros(ip.Results.trackLength, 1), ...
+        zeros(ip.Results.trackLength, 1), zeros(ip.Results.trackLength, 1), zeros(ip.Results.trackLength, 1), ...
+        zeros(ip.Results.trackLength, 1), zeros(ip.Results.trackLength, 1), ...
+        ones(ip.Results.trackLength, 1) * iParticle, ones(ip.Results.trackLength, 1) * iParticle,...
+        ones(ip.Results.trackLength, 1) * ip.Results.trackLength];
+
+        %Plot for verification
+%     plot(particlePos(:, 1), particlePos(:, 2))
+%     keyboard
+
+end
+
+%Generate the XLS output
+headers = {'frame', 'timestamp', 'x [nm]', 'y [nm]', 'z [nm]', ...
+    'x sigma [nm]', 'y sigma [nm]', 'z sigma [nm]', 'photons', ...
+    'bkg photons', 'track', 'track for matlab', 'track length'};
+writecell(headers, outputFN, 'FileType', 'spreadsheet', 'sheet', 'sim data');
+
+for iPD = 1:numel(particleData)
+
+    writematrix(particleData{iPD}, outputFN, 'FileType', 'spreadsheet', 'sheet', 'sim data', 'WriteMode', 'append')
+
+end
+
+end
