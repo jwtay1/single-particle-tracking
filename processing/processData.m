c04a58821f69434f99fc082bfb0b88043e32050e
Jian Tay <jian.tay@colorado.edu>
2022-05-10 13:34:37 -0600

    Added filter for immobile particles in processData

--- processing/processData.m
+++ processing/processData.m
@@ -2,8 +2,25 @@ clearvars
 clc
 
 trackData = readDataFromXLS(...
-    '../../data/spt 1s dark interval data worksheet.xlsx', ...
-    'MSC18 1s 2.0', 21);
+    '../../data/spt data worksheet D Histogram.xlsx', ...
+    'MSC18', 21);
+
+%Filter immobile particles
+minDisplacement = 50.1032;
+
+delInd = [];
+for iTrack = 1:numel(trackData)
+    
+    displacement = sqrt((trackData(iTrack).x(1) - trackData(iTrack).x(end))^2 + ...
+        (trackData(iTrack).y(1) - trackData(iTrack).y(end))^2 + ...
+        (trackData(iTrack).z(1) - trackData(iTrack).z(end))^2);
+
+    if displacement < (minDisplacement * 1e-3)
+        delInd(end + 1) = iTrack;
+    end
+
+end
+trackData(delInd) = [];
 
 [timeLag, squareDistances] = calculateSDandLagTime(trackData);
 
@@ -13,6 +30,7 @@ lt = unique(timeLag);
 %Compute the MSD
 for ii = 1:numel(lt)
     MSD(ii) = mean(squareDistances(timeLag == lt(ii)));
+    numTracks(ii) = nnz(timeLag == lt(ii));
 end
 
 %Fit MSD data to a line
@@ -20,3 +38,17 @@ fitData = fit(lt(1:4)', MSD(1:4)', 'poly1');
 
 %Compute the diffusion coefficient
 D = (1/(2 * 2)) * fitData.p1;
+
+%Plots
+figure(1)
+yyaxis left
+plot(lt, MSD)
+xlabel('Lag Time (s)')
+ylabel('MSD')
+yyaxis right
+plot(lt, numTracks)
+ylabel('Number of tracks')
+
+figure(2);
+histogram(squareDistances(timeLag == lt(1)), 500)
+
